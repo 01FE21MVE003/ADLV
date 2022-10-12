423d26b1935cbff12d41d24a7ca73da1ff9da465
Jamuna Chavadi <102353993+01FE21MVE003@users.noreply.github.com>
2022-10-12 12:43:16 +0530

    Update generator.sv

--- MEMORY MODEL/generator.sv
+++ MEMORY MODEL/generator.sv
@@ -1,34 +1,33 @@
 class generator;
   
-  //declaring transaction class
-  rand transaction trans;
-  
-  //declaring mailbox
-  mailbox gen2driv;
+  //declaring transaction class 
+  rand transaction trans,tr;
   
   //repeat count, to specify number of items to generate
-  int repeat_count;
+  int  repeat_count;
   
-  //event, to indicate the end of transaction generation
+  //mailbox, to generate and send the packet to driver
+  mailbox gen2driv;
+  
+  //event
   event ended;
   
   //constructor
-  function new(mailbox gen2driv, event ended);
-    
-  //getting the mailbox handle from env
-  this.gen2driv = gen2driv;
-  
+  function new(mailbox gen2driv,event ended);
+    //getting the mailbox handle from env, in order to share the transaction packet between the generator and driver, the same mailbox is shared between both.
+    this.gen2driv = gen2driv;
+    this.ended    = ended;
+    trans = new();
   endfunction
   
-    
-  //main task, generates the repeat_count number of transaction //packets into mailbox
-    task main();
-      repeat(repeat_count) begin
-        trans = new();
-        if(!trans.randomize())$fatal("Gen::trans randomization failed");
-        gen2driv.put(trans);
-      end
-      ->ended;//triggering indicates the end of generation
-    endtask
- 
+  //main task, generates(create and randomizes) the repeat_count number of transaction packets and puts into mailbox
+  task main();
+    repeat(repeat_count) begin
+    if( !trans.randomize() ) $fatal("Gen:: trans randomization failed");      
+    tr = trans.do_copy();
+    gen2driv.put(tr);
+    end
+    -> ended; 
+  endtask
+  
 endclass
