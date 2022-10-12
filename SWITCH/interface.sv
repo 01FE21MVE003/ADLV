90f8eb815e10641dc784de2c5494b0407adadde5
Jamuna Chavadi <102353993+01FE21MVE003@users.noreply.github.com>
2022-10-12 12:28:40 +0530

    Create interface.sv

---
+++ SWITCH/interface.sv
@@ -0,0 +1,10 @@
+interface intf(input logic clk,rstn);
+logic [7:0] addr;
+logic [15:0] data;
+logic [7:0] addr_a;
+logic [15:0] data_a;
+logic [7:0] addr_b;
+logic [15:0] data_a;
+logic vld;
+
+endinterface
